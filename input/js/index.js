b50199971d6bf9c6ea1e3451c7d79d9ae1eb840a
Steve Kemp <steve@steve.org.uk>
2014-02-25 10:25:11 +0000

    Updated to use e-comments rather than inline code.
      The external comment system was written for this site,
     but has been tidied up and released separately now.
    
      If users wish to clone this code then they'll be talking
     to the production comment-site, rather than a local instance
     anyway - as the comment-server endpoint is set to http://comments.../
     rather than "http://localhost...".
    
      On that basis storing an outdated copy of the comment-server
     is not doing anybody anything useful.

--- input/js/index.js
+++ input/js/index.js
@@ -1,117 +1,29 @@
-//
-//  Disquis-Lite - Inline comments via 100% Javascript/jQuery.
-//
-//  This script allows you to include comments in any static page,
-//  by making GET and POST requests against the URL:
-//
-//      http://comments.tweaked.io/comments/ID
-//
-//  To use this script add the following to the head of your HTML:
-//
-//  // begin
-//  <link rel="stylesheet" type="text/css" href="/css/c.css" media="screen" />
-//  <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-//  <script src="/js/index.js" type="text/javascript"></script>
-//  <script type="text/javascript">
-//     $( document ).ready(function() {
-//        discussion( "http://comments.tweaked.io/comments/id" );
-//     });
-//  </script>
-//  // end
-//
-//
-//  Once you've done that place a suitable DIV in your document body:
-//
-//    <div id="comments"></div>
-//
-//  et voila.
-//
-//  If comments are present they will be retrieved and displayed, otherwise
-// the user will be able to add them.  The comments are formatted via CSS
-// so you can change the look and feel easily, if you wish.
-//
-// Steve
-// --
-//
-
-
-//
-// Load the comments by making a JSONP request to the given URL.
-//
-// The comments will invoke the `comments(data)` function, when loaded.
-//
-function loadComments(url)
-{
-    $.ajax({
-        url: url + "?callback=?",
-        dataType:'jsonp',
-        crossDomain:true,
-	complete: (function(){populateReplyForm(url);})
-    });
-}
-
-
-//
-// Called when the JSONP data is loaded.
-//
-function comments(data)
-{
-    //
-    // We're given a DIV with ID comments.  Empty it.
-    //
-    //
-    $("#comments").html( "" );
-    id = 1
-
-    //
-    // If there are some comments we should post a header.
-    //
-    if ( data.length > 0 )
-    {
-        $("#comments").prepend( "<h2>Comments</h2>");
-    }
-
-    $.each(data,function( key,val) {
-
-        //
-        //  The variables from the comment
-        //
-        var author = val["author"];
-        var body   = val["body"];
-        var ago    = val["ago"];
-
-        $("#comments").append( "<div class=\"comment\"> \
-<div class=\"link\"><a href=\"#comment_" + id + "\">#</a></div> \
-<div class=\"title\"><a name=\"comment_" + id + "\">Author: " + author + "</a></div> \
-<div class=\"tagline\">" + ago + "<br /></div> \
-<div class=\"body\">" + body + "</div> \
-</div>" );
-	id += 1;
-    });
-}
-
-
-//
-// Generate the reply-form for users to add comments.
-//
-function populateReplyForm( url )
-{
-    //
-    //  Once the comments are loaded we can populate the reply-area.
-    //
-    $("#comments").append("<div id=\"comments-reply\"></div>" );
-
-    //
-    //  This is unpleasant.
-    //
-    $("#comments-reply").html(" \
-  <h2>Add Comment</h2> \
-  <blockquote> \
-<form method=\"POST\" id=\"myform\" action=\"\"> \
+function loadComments(url,options){$.ajax({url:url+"?callback=?",dataType:'jsonp',crossDomain:true,complete:(function(){populateReplyForm(url,options);})});}
+function comments(data){$("#comments").html("");id=1
+if(data.length>0){$("#comments").prepend("<h2>Comments</h2>");}
+$.each(data,function(key,val){var author=val["author"];var body=val["body"];var ago=val["ago"];$("#comments").append("<div class=\"comment\"> \
+<div class=\"link\"><a href=\"#comment_"+id+"\">#"+id+"</a></div> \
+<div class=\"title\"><a name=\"comment_"+id+"\">Author: "+author+"</a></div> \
+<div class=\"tagline\">Posted "+ago+".</div> \
+<div class=\"body\">"+body+"</div> \
+</div><div class=\"comment-spacing\"></div>");id+=1;});}
+function populateReplyForm(url,options){if(options&&options["reply-placement"]){if(options["reply-placement"]=="above"){$("#comments").prepend("<div id=\"comments-reply\"></div>");}
+else if(options["reply-placement"]=="below"){$("#comments").append("<div id=\"comments-reply\"></div>");}
+else{alert("Illegal 'reply-placement' value.   You may only use 'above' or 'below'");}}
+else{$("#comments").append("<div id=\"comments-reply\"></div>");}
+if(options&&options["reply-div"]){$("#comments-reply").html($(options["reply-div"]).html());}
+else{$("#comments-reply").html(" \
+<h2>Add Comment</h2> \
+<blockquote> \
+<form method=\"POST\" id=\"ecommentsreply\" action=\"\"> \
       <table> \
         <tr> \
           <td>Your Name</td> \
           <td><input type=\"text\" name=\"author\" /></td> \
+        </tr> \
+        <tr> \
+          <td>Your Email</td> \
+          <td><input type=\"text\" name=\"email\" /> (Optional)</td> \
         </tr> \
         <tr> \
           <td colspan=\"2\"><textarea name=\"body\" rows=\"5\" cols=\"50\"></textarea></td> \
@@ -123,54 +35,7 @@ function populateReplyForm( url )
       </table> \
     </form> \
   </blockquote> \
-");
-
-    //
-    // Capture form-submissions.
-    //
-    $("#myform").bind( "submit",(function() {
-
-        //
-        // Hide the form when submitting.
-        //
-	$("#myform").hide();
-
-        //
-        // Send the POST
-        //
-	$.ajax({
-            type: "POST",
-            url: url,
-            data: $("#myform").serialize(),
-            error: function(r,e)
-            {
-		loadComments( url);
-            },
-            complete: function(r,e)
-            {
-		loadComments( url );
-            },
-            datatype: 'jsonp',
-        })
-        return false;
-    }));
-
-}
-
-//
-// Entry point.
-//
-// Assumes the URL specified can be used to GET/POST comments, and has
-// the identifier associated with it already.
-//
-//  e.g. http://comments.tweaked.io/comments/apache
-//
-//
-function discussion(  url )
-{
-    //
-    //  Load the comments, and populate the <div id="comments"> area with them
-    //
-    loadComments( url )
-
-}
+");}
+$("#ecommentsreply").bind("submit",(function(){$("#ecommentsreply").hide();$.ajax({type:"POST",url:url,data:$("#ecommentsreply").serialize(),error:function(r,e){loadComments(url,options);},complete:function(r,e){loadComments(url,options);},datatype:'jsonp',})
+return false;}));}
+function discussion(url,options){loadComments(url,options)}
